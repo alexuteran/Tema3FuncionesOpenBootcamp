26fd604538bf36374125a7ba4d5a97ecafdd9d79
alexander uzcategui <publiestructurasrl@gmail.com>
2022-11-28 15:21:38 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+         int n1=0,n2=0,n3=0,resultado=0;
+
+         resultado = suma(n1=3, n2=4, n3=5);
+         System.out.println( "El resultado de la suma es: "+ resultado);
+    }
+
+    public static int suma(int n1, int n2, int n3){
+         return n1+n2+n3;
+    }
+}
